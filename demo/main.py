a2e8b3c943cc5ee57192d5f4711aab1f70ae72c0
Vaibhav <76144239+JuniorfoxDev@users.noreply.github.com>
2024-04-08 11:09:25 +0530

    Update main.py

--- demo/main.py
+++ demo/main.py
@@ -8,7 +8,7 @@ from utils import *
 
 def main():
     st.title("Sanjevani")
-    st.markdown('<h5 style="text-align:center;>Retinal Disease Detection</h5>')
+    st.markdown('Retinal Disease Detection')
 
     # Setting device agnostic code
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
